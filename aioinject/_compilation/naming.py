b9b8913424ae3bd8d9ea2baea65b07eb28477a8d
notypecheck <50728601+notypecheck@users.noreply.github.com>
2025-06-20 14:57:40 +0300

    fix: generate valid variable name with unions

--- aioinject/_compilation/naming.py
+++ aioinject/_compilation/naming.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import types
 import typing
 from collections.abc import Sequence
 from typing import TYPE_CHECKING
@@ -16,10 +17,13 @@ def create_var_name(node: AnyNode) -> str:
 def make_dependency_name(type_: type[object]) -> str:
     args = typing.get_args(type_)
     type_id = id(type_ if not args else typing.get_origin(type_))
-
     if not args:
         return f"{type_.__name__}_{type_id}"
-    args_str = "_".join(arg.__name__ for arg in args)
+
+    args_str = "_".join(make_dependency_name(arg) for arg in args)
+    if typing.get_origin(type_) == types.UnionType:
+        return args_str
+
     return f"{type_.__name__}_{args_str}_{type_id}"
 
 
